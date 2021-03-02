5dca41607e80252af303a580f0c79f8d0fb66fdc
Reid Kuttler <reidkuttler@gmail.com>
2021-03-02 15:54:52 -0700

    added github link

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,10 @@ export default class Footer extends React.Component {
 
     render() {
         return (
-            <div>Footer</div>
+            <div>
+                <div>Footer</div>
+                <div><a href="https://github.com/reiddk/blueowl-interview">https://github.com/reiddk/blueowl-interview</a></div>
+            </div>
         );
     }
 }
